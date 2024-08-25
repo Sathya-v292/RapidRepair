49434c881d89c46af4cabb471902260a9737c857
sathya-v929 <veluselladurai5@gmail.com>
2024-08-25 11:13:21 +0530

    blue theme register page created

--- app/src/main/java/com/example/rapidrepair/MainActivity.java
+++ app/src/main/java/com/example/rapidrepair/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.rapidrepair;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -12,13 +16,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-
-        });
+    }
+    public void r1(View V){
+        Intent i = new Intent(this,Register.class);
+        startActivity(i);
     }
 }
